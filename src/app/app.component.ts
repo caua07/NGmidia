44d49789bafeefb7ae466cb828ef70f061b18aa8
caua07 <ratauzim@gmail.com>
2024-02-23 18:30:03 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './test/test.component.html',
+  styleUrl: './test/test.component.scss'
 })
+
+
 export class AppComponent {
   title = 'LpNg';
+
+  constructor(private router: Router) { }
+
+  entrarSite() {
+    console.log("Navegando para a rota...");
+    this.router.navigate(['/src/app/landing/landing.component.html']);
+  }
 }
---
+++ src/app/test/test.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.scss'
+})
+export class TestComponent {
+
+
+
+}
